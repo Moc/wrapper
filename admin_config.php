b7fddef48030a0b876f944376d8a3d5730086d05
Tijn Kuyper <github@tijnkuyper.nl>
2015-12-07 14:46:40 +0100

    Fixes #2 - Added info message with URL. Also, clean-up

--- admin_config.php
+++ admin_config.php
@@ -9,7 +9,7 @@
  */
 
 require_once('../../class2.php');
-if (!getperms('P')) 
+if (!getperms('P'))
 {
 	header('location:'.e_BASE.'index.php');
 	exit;
@@ -17,17 +17,17 @@ if (!getperms('P'))
 
 class wrapper_adminArea extends e_admin_dispatcher
 {
-	protected $modes = array(	
-	
+	protected $modes = array(
+
 		'main'	=> array(
 			'controller' 	=> 'wrapper_ui',
 			'path' 			=> null,
 			'ui' 			=> 'wrapper_form_ui',
 			'uipath' 		=> null
 		),
-	);	
-	
-	
+	);
+
+
 	protected $adminMenu = array(
 		'main/list'			=> array('caption'=> LAN_MANAGE, 'perm' => 'P'),
 		'main/create'		=> array('caption'=> LAN_CREATE, 'perm' => 'P'),
@@ -35,31 +35,31 @@ class wrapper_adminArea extends e_admin_dispatcher
 	);
 
 	protected $adminMenuAliases = array(
-		'main/edit'	=> 'main/list'				
-	);	
-	
+		'main/edit'	=> 'main/list'
+	);
+
 	protected $menuTitle = 'Wrapper';
 }
 
-			
+
 class wrapper_ui extends e_admin_ui
-{			
+{
 		protected $pluginTitle		= 'Wrapper';
 		protected $pluginName		= 'wrapper';
-	//	protected $eventName		= 'wrapper-wrapper'; // remove comment to enable event triggers in admin. 		
+	//	protected $eventName		= 'wrapper-wrapper'; // remove comment to enable event triggers in admin.
 		protected $table			= 'wrapper';
 		protected $pid				= 'wrapper_id';
-		protected $perPage			= 10; 
+		protected $perPage			= 10;
 		protected $batchDelete		= true;
-	//	protected $batchCopy		= true;		
+	//	protected $batchCopy		= true;
 	//	protected $sortField		= 'somefield_order';
 	//	protected $orderStep		= 10;
-	//	protected $tabs				= array('Tabl 1','Tab 2'); // Use 'tab'=>0  OR 'tab'=>1 in the $fields below to enable. 
-		
+	//	protected $tabs				= array('Tabl 1','Tab 2'); // Use 'tab'=>0  OR 'tab'=>1 in the $fields below to enable.
+
 	//	protected $listQry      	= "SELECT * FROM `#tableName` WHERE field != '' "; // Example Custom Query. LEFT JOINS allowed. Should be without any Order or Limit.
-	
+
 		protected $listOrder		= 'wrapper_id DESC';
-	
+
 		protected $fields 		= array (  'checkboxes' =>   array ( 'title' => '', 'type' => null, 'data' => null, 'width' => '5%', 'thclass' => 'center', 'forced' => '1', 'class' => 'center', 'toggle' => 'e-multiselect',  ),
 		  'wrapper_id' 			=>  array ( 'title' => LAN_ID, 		'data' => 'int', 'width' => '5%', 'help' => '', 'readParms' => '', 'writeParms' => '', 'class' => 'left', 'thclass' => 'left',  ),
 		  'wrapper_title' 		=>  array ( 'title' => LAN_TITLE, 	'type' => 'text', 'data' => 'str', 'width' => 'auto', 'inline' => true, 'help' => LAN_WRAPPER_TITLE_HELP, 'readParms' => '', 'writeParms' => '', 'class' => 'left', 'thclass' => 'left',  ),
@@ -67,36 +67,47 @@ class wrapper_ui extends e_admin_ui
 		  'wrapper_height'		=>  array ( 'title' => LAN_WRAPPER_HEIGHT, 'type' => 'text', 'data' => 'str', 'width' => 'auto', 'inline' => true, 'help' => LAN_WRAPPER_HEIGHT_HELP, 'readParms' => '', 'writeParms' => '', 'class' => 'left', 'thclass' => 'left',  ),
 		  'wrapper_width' 		=>  array ( 'title' => LAN_WRAPPER_WIDTH, 'type' => 'text', 'data' => 'str', 'width' => 'auto', 'inline' => true, 'help' => LAN_WRAPPER_WIDTH_HELP, 'readParms' => '', 'writeParms' => '', 'class' => 'left', 'thclass' => 'left',  ),
 		  //'wrapper_auto_height' =>  array ( 'title' => 'Auto height', 'type' => 'boolean', 'data' => 'int', 'width' => 'auto', 'help' => 'Let the plugin choose the appropriate height', 'readParms' => '', 'writeParms' => '', 'class' => 'left', 'thclass' => 'left',  ),
-		  'wrapper_scrollbars' =>  array ( 'title' => LAN_WRAPPER_SCROLLBARS, 'type' => 'boolean', 'data' => 'int', 'width' => 'auto', 'help' =>  LAN_WRAPPER_SCROLLBARS_HELP, 'readParms' => '', 'writeParms' => '', 'class' => 'left', 'thclass' => 'left',  ),
+		  'wrapper_scrollbars' 	=>  array ( 'title' => LAN_WRAPPER_SCROLLBARS, 'type' => 'boolean', 'data' => 'int', 'width' => 'auto', 'help' =>  LAN_WRAPPER_SCROLLBARS_HELP, 'readParms' => '', 'writeParms' => '', 'class' => 'left', 'thclass' => 'left',  ),
 		  'wrapper_userclass'	=>  array ( 'title' => LAN_USERCLASS, 'type' => 'userclass', 'data' => 'int', 'width' => 'auto', 'inline' => true, 'filter' => true, 'help' => LAN_WRAPPER_USERCLASS_HELP, 'readParms' => '', 'writeParms' => '', 'class' => 'left', 'thclass' => 'left',  ),
 		  'options' 			=>  array ( 'title' => LAN_OPTIONS, 'type' => null, 'data' => null, 'width' => '10%', 'thclass' => 'center last', 'class' => 'center last', 'forced' => '1',  ),
-		);		
-		
+		);
+
 		protected $fieldpref = array('wrapper_id', 'wrapper_title', 'wrapper_url', 'wrapper_height', 'wrapper_width', 'wrapper_scrollbars', 'wrapper_userclass');
 		// 'wrapper_auto_height',
 
 	//	protected $preftabs        = array('General', 'Other' );
 		protected $prefs = array(
-		); 
+		);
 
-	
 		public function init()
 		{
+			$action	= $this->getAction();
+			$id 	= $this->getId();
+
+			if($action == 'edit')
+			{
+				$url = SITEURLBASE.e_PLUGIN_ABS."wrapper/wrapper.php?".$id;
+				$link = '<a href="'.$url.'">'.$url.'</a>';
+				e107::getMessage()->addInfo(LAN_WRAPPER_INFOMESSAGE.": ".$link);
+			}
 		}
 
 		// ------- Customize Create --------
 		public function beforeCreate($new_data)
 		{
 		}
-	
+
 		public function afterCreate($new_data, $old_data, $id)
 		{
+			$url = SITEURLBASE.e_PLUGIN_ABS."wrapper/wrapper.php?".$id;
+			$link = '<a href="'.$url.'">'.$url.'</a>';
+			e107::getMessage()->addSuccess(LAN_WRAPPER_INFOMESSAGE.": ".$link);
 		}
 
 		public function onCreateError($new_data, $old_data)
 		{
-		}		
-		
+		}
+
 		// ------- Customize Update --------
 		public function beforeUpdate($new_data, $old_data, $id)
 		{
@@ -105,20 +116,19 @@ class wrapper_ui extends e_admin_ui
 		public function afterUpdate($new_data, $old_data, $id)
 		{
 		}
-		
+
 		public function onUpdateError($new_data, $old_data, $id)
 		{
-		}		
-	
+		}
+
 		/*
-		// optional - a custom page.  
+		// optional - a custom page.
 		public function customPage()
 		{
 			$text = 'Hello World!';
 			return $text;
 		}
 		*/
-	
 
 	public function renderHelp()
 	{
@@ -131,13 +141,13 @@ class wrapper_ui extends e_admin_ui
 			'caption'	=> "Help",
 			'text'		=> $text,
 		);
-	}		
+	}
 }
-				
+
 class wrapper_form_ui extends e_admin_form_ui
 {
-}			
-		
+}
+
 new wrapper_adminArea();
 
 require_once(e_ADMIN."auth.php");
