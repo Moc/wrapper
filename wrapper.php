a2707c67ab0a3af639d9cc7ebf6d9590a1436abb
Tijn Kuyper <github@tijnkuyper.nl>
2015-12-07 14:45:52 +0100

    Clean-up and LAN fix

--- wrapper.php
+++ wrapper.php
@@ -10,7 +10,7 @@
 
 require_once("../../class2.php");
 
-// Make this page inaccessible when plugin is not installed. 
+// Make this page inaccessible when plugin is not installed.
 if (!e107::isInstalled('wrapper'))
 {
 	header('location:'.e_BASE.'index.php');
@@ -18,29 +18,29 @@ if (!e107::isInstalled('wrapper'))
 }
 
 // Load the LAN files and set initial variables
-e107::lan('oweme', false, true); 
+e107::lan('wrapper', false, true); 
 $caption = "Wrapper";
-$error = ''; 
+$error = '';
 
 require_once(HEADERF);
 
 // Retrieve query and check for wrap_pass
 list($id, $wrap_pass) = explode('&amp;wrap_pass=', e_QUERY, 2);
 
-if(!empty($wrap_pass)) 
+if(!empty($wrap_pass))
 {
 	$wrap_pass = '?'.$wrap_pass;
 }
 
-// Check for Wrapper ID in the URL. Display error if no ID is found in the URL. 
+// Check for Wrapper ID in the URL. Display error if no ID is found in the URL.
 if(!varset($id))
 {
-	$error = e107::getMessage()->addError(LAN_WRAPPER_ERR1); 
+	$error = e107::getMessage()->addError(LAN_WRAPPER_ERR1);
 }
 // Check for ID validity. Display error if ID is not found in the database.
 elseif(!$sql->select("wrapper", "*", "wrapper_id='$id'"))
 {
-	$error = e107::getMessage()->addError(LAN_WRAPPER_ERR2); 
+	$error = e107::getMessage()->addError(LAN_WRAPPER_ERR2);
 }
 
 // If error is found, display error message and exit.
@@ -55,7 +55,7 @@ if($error)
 else
 {
 	$wrapper_query = $sql->retrieve('wrapper', '*', 'wrapper_id='.$id);
-	
+
 	$caption = $wrapper_query['wrapper_title'];
 
 	// Check for userclass access
@@ -68,14 +68,14 @@ else
 	}
 
 	// Convert scrollbars DB value to HTML values
-	$scrollbars = ($wrapper_query['wrapper_scrollbars'] == 1 ? 'yes' : 'no'); 
+	$scrollbars = ($wrapper_query['wrapper_scrollbars'] == 1 ? 'yes' : 'no');
 
 	// Check to see if the auto height was selected and if yes, insert the appropriate JS in the footer
-	// NOT WORKING ANYMORE DUE TO CHANGING BROWSER STANDARDS. WORK IN PROGRESS BELOW. 
+	// NOT WORKING ANYMORE DUE TO CHANGING BROWSER STANDARDS. WORK IN PROGRESS BELOW.
 	/*if($wrapper_query['wrapper_auto_height'])
 	{
 		$onload = 'onload="iFrameHeight()"';
-		
+
 		e107::js('footer-inline', "
  				function iFrameHeight() {
                         var h = 0;
@@ -92,26 +92,26 @@ else
 
 	<iframe ".$onload." src='{$wrapper_query['wrapper_url']}{$wrap_pass}' id='blockrandom' name='iframe' width='{$wrapper_query['wrapper_width']}%' height='{$wrapper_query['wrapper_height']}' scrolling='{$wrapper_query['wrapper_scroll_bars']}' frameborder='0' marginheight='0' marginwidth='0'><br />".WRAP_4."<a href='{$page}'>".WRAP_5."</a>.<br /></iframe>";
 	*/
-	
-	// Check if width and/or wrapper height is set 
+
+	// Check if width and/or wrapper height is set
 	if(
 		isset($wrapper_query['wrapper_width']) ||
 		isset($wrapper_query['wrapper_height']) ||
-		(isset($wrapper_query['wrapper_height']) && isset($wrapper_query['wrapper_width']))  
+		(isset($wrapper_query['wrapper_height']) && isset($wrapper_query['wrapper_width']))
 	  )
-	{	
+	{
 		// Set default width and height, if they are not already defined
 		$width  = varsettrue($wrapper_query['wrapper_width'], "100%");
 		$height = varsettrue($wrapper_query['wrapper_height'], "800");
 
-		$text = "<iframe src='".$wrapper_query['wrapper_url'].$wrap_pass."' width='".$width."' height='".$height."' scrolling='".$scrollbars."' frameborder='0'></iframe>"; 
+		$text = "<iframe src='".$wrapper_query['wrapper_url'].$wrap_pass."' width='".$width."' height='".$height."' scrolling='".$scrollbars."' frameborder='0'></iframe>";
 	}
 	// No width and height set, display the iframe fullscreen
 	else
 	{
 		$width  = $wrapper_query['wrapper_width'];
 		$height = $wrapper_query['wrapper_height'];
-		$text = "<iframe src='".$wrapper_query['wrapper_url'].$wrap_pass."' width='100%' style='height: 100em;' scrolling='".$scrollbars."' frameborder='0'></iframe>"; 
+		$text = "<iframe src='".$wrapper_query['wrapper_url'].$wrap_pass."' width='100%' style='height: 100em;' scrolling='".$scrollbars."' frameborder='0'></iframe>";
 	}
 }
 
