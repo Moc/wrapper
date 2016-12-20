90b0888a2d4d415e5232edabb034f18f9b3c4fca
Gábor <yesszus@gmail.com>
2016-12-20 12:06:09 +0100

    Add files via upload

---
+++ languages/Hungarian/Hungarian_front.php
@@ -0,0 +1,16 @@
+<?php
+/*
++---------------------------------------------------------------+
+|        e107 website content management system Hungarian Language File
+|        Released under the terms and conditions of the
+|        GNU General Public License (http://gnu.org).
+|        Last Modified: 2016/12/20 11:15:43
+|
+|        $Author: Yesszus $
++---------------------------------------------------------------+
+*/
+
+define("LAN_WRAPPER_ERR1", "A keret ID nem található! Kérjük, ellenőrizze az URL-t vagy a shortcode helyességét!"); 
+define("LAN_WRAPPER_ERR2", "Érvénytelen keret ID! Kérjük, ellenőrizze az URL-t vagy a shortcode helyességét!"); 
+define("LAN_WRAPPER_ERR3", "Ön nem jogosult az oldal megtekintésére!");
+?>
