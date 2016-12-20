d1d613155a836c9bcba27f6bed44b6b2d05b0e80
Gábor <yesszus@gmail.com>
2016-12-20 12:05:34 +0100

    Create Hungarian_admin.php

---
+++ languages/Hungarian/Hungarian_admin.php
@@ -0,0 +1,28 @@
+<?php
+/*
++---------------------------------------------------------------+
+|        e107 website content management system Hungarian Language File
+|        Released under the terms and conditions of the
+|        GNU General Public License (http://gnu.org).
+|        Last Modified: 2016/12/20 11:15:43
+|
+|        $Author: Yesszus $
++---------------------------------------------------------------+
+*/
+
+define("LAN_WRAPPER_HEIGHT", 	"Magasság");
+define("LAN_WRAPPER_WIDTH", 	"Szélesség");
+define("LAN_WRAPPER_SCROLLBARS","Görgetés");
+
+// HELP
+define("LAN_WRAPPER_TITLE_HELP", 	"A weboldal neve (nem szükséges)");
+define("LAN_WRAPPER_URL_HELP", 		"A külső oldal URL címe, amit szeretne megjeleníteni.");
+define("LAN_WRAPPER_HEIGHT_HELP", 	"A keret magassága pixelben. Hagyja üresen vagy adja meg a 0 számot a teljes képernyős megjelenítéshez.");
+define("LAN_WRAPPER_WIDTH_HELP", 	"A keret szélessége pixelben. Hagyja üresen vagy adja meg a 0 számot a teljes képernyős megjelenítéshez.");
+define("LAN_WRAPPER_SCROLLBARS_HELP", "A gördítősáv megjelenítésének engedélyezése, letiltása.");
+define("LAN_WRAPPER_USERCLASS_HELP", "Válassza ki a csoportot, akik láthatják az oldalt.");
+
+define("LAN_WRAPPER_URL", "A keret URL [x] vagy [y]");
+
+define("LAN_WRAPPER_HELP", "További információkért olvassa el a [dokumentációt].");
+?>
