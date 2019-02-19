b84b16b985637914ccbe169d9de50e12356e3ff3
Tijn Kuyper <github@tijnkuyper.nl>
2019-02-19 10:44:35 +0100

    Remove copyright text

--- languages/English/English_global.php
+++ languages/English/English_global.php
@@ -1,12 +1,5 @@
 <?php
-/*
- * Wrapper - an e107 plugin by Tijn Kuyper
- *
- * Copyright (C) 2015-2016 Tijn Kuyper (http://www.tijnkuyper.nl)
- * Released under the terms and conditions of the
- * GNU General Public License (http://www.gnu.org/licenses/gpl.txt)
- *
- */
+
 define("LAN_WRAPPER_NAME",  "Wrapper");
 define("LAN_WRAPPER_DESC",  "Wrapping an external page into your website.");
 define("LAN_WRAPPER_SUMM",  "This plugin allows for wrapping an external page into an iframe, so it can be displayed on your website.");
